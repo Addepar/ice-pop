4625f330fa9ea6f02e7321122410c891fc100a24
Christopher Garrett <me@pzuraq.com>
2017-07-17 20:11:06 -0700

    refactor(lint): Updates lint packages and other deps

--- testem.js
+++ testem.js
@@ -1,14 +1,15 @@
 /* eslint-env node */
 module.exports = {
-  "test_page": "tests/index.html?hidepassed",
-  "disable_watching": true,
-  "reporter": "xunit",
-  "xunit_intermediate_output": true,
-  "report_file": "testlogs/results.xml",
-  "launch_in_ci": [
-    "Chrome"
+  framework: 'qunit',
+  test_page: 'tests/index.html?hidepassed',
+  launch_in_ci: [
+    'Chrome'
   ],
-  "launch_in_dev": [
-    "Chrome"
-  ]
+  launch_in_dev: [
+    'Chrome'
+  ],
+  reporter: 'xunit',
+  xunit_intermediate_output: true,
+  browser_start_timeout: 300,
+  report_file: 'test-results.xml'
 };
