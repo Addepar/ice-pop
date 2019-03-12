567ce30d78a3954f968acacd1a57bbd88cf17cf1
Cyril Fluck <cyril.fluck@addepar.com>
2019-03-12 11:54:14 -0700

    dropdown not only on click

--- addon/components/adde-dropdown.js
+++ addon/components/adde-dropdown.js
@@ -48,7 +48,7 @@ import layout from '../templates/components/adde-dropdown';
 export default class AddeDropdownComponent extends BasePopMenuComponent {
   layout = layout;
 
-  triggerEvent = 'click';
+  // triggerEvent = 'click';
 
   // ----- Public Settings ------
 
